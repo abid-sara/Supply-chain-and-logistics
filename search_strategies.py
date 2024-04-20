81e20349b9fd72e4a89463e9bd58124277d9e212
SabrinaBouchekhlal <146650283+SabrinaBouchekhlal@users.noreply.github.com>
2024-04-20 08:52:31 -0700

    Update search_strategies.py

--- search_strategies.py
+++ search_strategies.py
@@ -34,45 +34,87 @@ def breadth_first_search(problem, initial_states):
 
 
 # ucs starts here
-def ucs_helper(problem, initial_state):
-    """Uniform Cost Search algorithm"""
-    frontier = [(0, initial_state, [(initial_state, 0)])]  # (total_cost, state, path)
-    explored = {}  # Explored nodes and their costs
+def ucs_helper(problem):
+    frontier = []
+    explored = set()
+    initial_node = Node(problem.state)
+   # heapq.heappush(frontier, (0, initial_node))
+    frontier.append((initial_node.cost, initial_node))  # Initial node with cost 0
+    '''The frontier is implemented as a priority queue using the heapq module in Python. This ensures that the node with the smallest cumulative cost is always popped from the frontier first.'''
 
     while frontier:
-        # Sort the frontier by total_cost
-        frontier.sort(key=lambda x: x[0])
-        # Get the node with the lowest cost
-        total_cost, node, path = frontier.pop(0)
-        if problem.is_goal_test(Node(node)):  # Check if the current node is the goal state
-            return path, total_cost
-
-        # Check if the node has already been explored or if the new path has a lower cost
-        if node not in explored or total_cost < explored[node][0]:
-            explored[node] = (total_cost, path)  # This ensures that the algorithm keeps track of the lowest-cost path to each explored state.
-
-            # Retrieve neighbors and their costs from the problem
-            children = problem.expand_node(Node(node))
-
-            for child in children:
-                new_total_cost = total_cost + child.cost
-                new_path = path + [(child.state, child.cost)]
-                child_node = (new_total_cost, child.state, new_path)
-                frontier.append(child_node)
-
-    return None, 0  # return path, total_cost and returns (None, 0) if no solution is found
+        frontier.sort(key=lambda x: x[0])  # Sort the frontier by cost
+        current_cost, node = frontier.pop(0)  # Pop the node with the lowest cost
+        #current_cost, node = heapq.heappop(frontier)
+        if problem.is_goal_test(node):
+            return node
+        if node not in explored:
+         explored.add(node.state)
+         children = problem.expand_node(node)
+         for child in children:
+            if child.state not in explored:
+                #heapq.heappush(frontier, (child_cost, child))
+                frontier.append((child.cost, child))# child node have accumulated cost its cost + parent cost (see expand_node function)
 
 
-def ucs(problem, initial_states):
-    solution_set = set()
+    return None
+def uniform_cost_search(problem, initial_states):
+    solutions = []
     for initial_state in initial_states:
-        solution, total_cost = ucs_helper(problem, initial_state)
-        solution = tuple(solution)
-        if solution:
-            solution_set.add((solution, total_cost))
-    return solution_set
-#ucs finishes here
+        problem.state = initial_state
+        solution_node = ucs_helper(problem)
+        if solution_node:
+            solution_path = []
+            node = solution_node
+            while node:
+                solution_path.insert(0, node)
+                node = node.parent
+            #solution_path.reverse()
+            solutions.append(solution_path)
+    return solutions
 
+
+def UcsCompany(problem,initial_states,companies):
+    setSol=uniform_cost_search(problem,initial_states)
+    min_cost1 = float('inf')
+    min_cost_final = float('inf')
+    min_path = None#path from source city to user wilaya
+    best_company = None
+    best_path = None#final path
+    min_initial_state = None#goalstate of ucscompanyhelper
+
+    for path1 in setSol:
+        cost1=path1[-1].cost #access the  last node (the goal) and get the path cosy
+
+        if cost1 < min_cost1:
+            min_cost1 = cost1
+            min_path = [node.state for node in path1]  # Convert the path to a list of states
+            # Since each (path, cost) corresponds to a specific initial_state in setSol,
+            # we don't need to track the initial_state here directly.
+            min_initial_state = min_path[0]  # Assuming path is not empty
+    #goal_state_of_company=min_initial_state#source_city
+    #initial_states_company=companies
+    setComp=[]#initial_states for ucs companies
+    
+    setComp = [companies[company]['wilaya'] for company in companies]
+
+    problem.set_goal(min_initial_state)#update the problem goal
+
+    setSolCompanies=uniform_cost_search(problem,setComp)
+
+    for company, path_company_source in zip(companies, setSolCompanies):
+        cost_company_source = path_company_source[-1].cost  # Get the cost of the last node in the path(the goal so the cost of reaching the goal)
+        cost2=min_cost1+cost_company_source #the total cost of transporting the product from the company to the wilaya of user 
+        
+        if cost2 <min_cost_final:
+            min_cost_final = cost2
+            best_company = company
+            best_path = [node.state for node in path_company_source[:-1]] + min_path  # the final path of transporting the product from the company to the wilaya of user 
+    
+    if min_cost_final == float('inf'):
+        return None, None, None ,None,None
+
+    return best_company, min_cost_final, best_path ,min_initial_state,companies[best_company]['wilaya']
 
 # informed search functions start here
 def a_star_helper(problem, initial_states_product):
@@ -140,11 +182,20 @@ with open('data/wilaya_frontiere.txt', 'r') as file:
             state_transition_model[city]["neighbors"][neighbor_city] = distance
 
 
-problemLogistic = TransportProblem("", "Alger", state_transition_model)
-cities = problemLogistic.find_source_city("tomato", "52384000", "6")
-companies = problemLogistic.find_company("tomato", "52384000")
+target_city = input("Enter the city you want to ship to: ")
+product = input("Enter product: ")
+quantity = input("Enter quantity in kg: ")
+month = input("Enter the month of the shipment: ")
+problemLogistic = TransportProblem("", target_city, state_transition_model)
+cities = problemLogistic.find_source_city(product, quantity, month)
+companies = problemLogistic.find_company(product, quantity)
 
+best_company, min_cost, best_path ,min_initial_state,wilaya_company=UcsCompany(problemLogistic,cities,companies)
+print(f"source_city : {min_initial_state}")
+print(f"The best company is {best_company} in wilaya {wilaya_company}")
 
+print(f"The minimum cost is {min_cost}")
+print(f"path : {best_path}")
 # calling the functions and printing (will be changed with a menu eventually)
 # A star
 '''
@@ -182,3 +233,4 @@ else:
     print("No solution found for any of the initial states.")
 
 print("------------------------------------"*4)
+'''
